2e2dffea09e7d9bd8f0319436843d77a2cdd7537
Mairon198 <omarlicona198@gmail.com>
2025-03-06 20:58:28 -0600

    Agregando Tarea No. 1, Ejercicio No. 6

---
+++ src/main/java/com/mycompany/ejercicio7/Ejercicio7.java
@@ -0,0 +1,17 @@
+//Mairon Omar Licona Duarte
+//201810010200
+//Escribe un programa que imprima mediante un ciclo los números pares del 2 al 100
+
+
+package com.mycompany.ejercicio7;
+
+
+public class Ejercicio7 {
+
+   public static void main(String[] args) {
+       
+        for (int c=2; c<=100; c+=2) {
+            System.out.println(c);
+        }
+    }
+}
